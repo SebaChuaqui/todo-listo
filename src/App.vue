5a520c616601d13242441465bfe3d2acea48bada
SebaChuaqui <schuaquicano@gmail.com>
2022-06-09 19:36:30 -0300

    todo-listo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,92 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="tarea-lista">
+    <h1>{{tareas.length}} TAREAS</h1>
+    <form class="form" @submit.prevent="crearTarea">
+      <label class="label" for="tarea">Tarea Nueva:</label>
+      <input class="input" type="text" v-model="nuevaTarea" id="tarea" />
+      <input class="button" type="submit" value="Crear tarea" />
+    </form>
+    <ol class="lista">
+      <li
+        class="tarea"
+        v-for="(tarea, x) in tareas"
+        :key="'tarea' + x"
+        :class="{complete: tarea.complete}"
+        @click="completeTarea(tarea.text)"
+      >{{tarea.text}}</li>
+    </ol>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data: () => ({
+    nuevaTarea: "",
+    tareas: []
+  }),
+  methods: {
+    crearTarea() {
+      let tarea = {
+        text: this.nuevaTarea,
+        complete: false
+      };
+      this.tareas.push(tarea);
+      this.nuevaTarea = "";
+      console.log(this.tareas);
+    },
+    completeTarea(tareaTexto) {
+      for (let x = 0; x < this.tareas.length; x++) {
+        let tarea = this.tareas[x];
+        if (tareaTexto === tarea.text) {
+          tarea.complete = !tarea.complete;
+        }
+      }
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.tarea-lista {
+  width: 800px;
+  max-width: 100%;
+  margin: 0px auto;
 }
+.form {
+  background: orangered;
+  border-radius: 12px;
+  padding: 30px;
+  box-shadow: 0px 10px 22px -1px rgba(0,0,0,0.25);
+  margin-top: 10px;
+}
+.label {
+  display: block;
+  margin-bottom: 10px;
+  font-weight: bold;
+  
+}
+.input {
+  height: 35px;
+}
+.button {
+  margin-left: 20px;
+  height: 35px;
+  border: none;
+  border-radius: 5px;
+  box-shadow: 0 1px 4px rgba(0, 0, 0, .2);
+  background-color: black;
+  color: yellow;
+  cursor: pointer
+}
+.lista {
+  margin-top: 40px;
+}
+.tarea {
+  cursor: pointer;
+  margin: 10px 0;
+}
+.complete {
+  text-decoration: line-through;
+  color: lightgrey;
+}
+
 </style>
